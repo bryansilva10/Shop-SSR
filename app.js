221d9643d1f80ad5c266e2bc574bb6a4caed9208
Bryan Guillen <bryanguillen@Bryans-MacBook-Pro.local>
2020-06-28 16:58:31 -0600

    Another fix

--- app.js
+++ app.js
@@ -126,12 +126,18 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-	// res.status(error.httpStatusCode).render(...);
-	// res.redirect('/500');
+	let isl = false;
+	if (req) {
+		if (req.session) {
+			if (req.session.isLoggedIn) {
+				isl = req.session.isLoggedIn
+			}
+		}
+	}
 	res.status(500).render('500', {
 		pageTitle: 'Error!',
 		path: '/500',
-		isAuthenticated: req.session.isLoggedIn
+		isAuthenticated: isl
 	});
 });
 
